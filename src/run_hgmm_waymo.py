97dd40cae20bc96111128f1a9c5180165d96bfb4
botforge <dhruvkar@seas.upenn.edu>
2019-12-02 00:30:26 -0500

    HGMM Localization Works

--- src/run_hgmm_waymo.py
+++ src/run_hgmm_waymo.py
@@ -8,24 +8,26 @@ from probreg import callbacks
 from collections import deque
 import hgmm_utils
 
-waymopair = WaymoLIDARPair(max_frames=50, as_pc = True, voxel_size = 2.0, filename='/home/mlab-dhruv/Desktop/pointclouds/waymodata/segment-10206293520369375008_2796_800_2816_800_with_camera_labels.tfrecord')
-TRANSLATE_02x = np.identity(4)
-TRANSLATE_02x[0, 3] = 0.5
+waymopair = WaymoLIDARPair(max_frames=150, as_pc = True, voxel_size = 0.5, filename='/home/mlab-dhruv/Desktop/pointclouds/waymodata/segment-10206293520369375008_2796_800_2816_800_with_camera_labels.tfrecord')
 
 done = False
-all_trans = deque([])
+all_trans = []
 vis = WaymoLIDARVisCallback() 
+idx = 0
 while True:
     prev_np, curr_np, prev_pc, curr_pc, done = waymopair.next_pair()
     if done:
         break
 
     #1: Compute L2 Gaussian Registration & Transform chain
-    tf_param = l2dist_regs.registration_svr(curr_pc, prev_pc)
-    all_trans.append(tf_param)
-    result = copy.deepcopy(curr_pc)
-    # result.points = tf_param.transform(result.points)
-
-    for tf in all_trans.reverse():
+    tf_param = l2dist_regs.registration_svr(prev_pc, curr_pc)
+    all_trans.append(copy.deepcopy(tf_param.inverse()))
+    if idx == 0:
+        result = copy.deepcopy(curr_pc)
+    else:
+        result = convert_np_to_pc(np.linspace(-0.03, 0.01, num=20*3).reshape(-1, 3))
+        result.colors = o3.utility.Vector3dVector(np.ones((20*3)).reshape(-1, 3) * np.array([1, 0, 0]))
+    # idx+=1
+    for tf in reversed(all_trans):
         result.points = tf.transform(result.points)
     vis(result, addpc=True)
